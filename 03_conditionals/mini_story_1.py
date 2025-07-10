ea6005aa97955a8f1381abe1a35566d6a51edfe7
Rishabh Sharma <147262906+Hactivis@users.noreply.github.com>
2025-07-10 18:52:52 +0530

    Add files via upload

---
+++ 03_conditionals/mini_story_1.py
@@ -0,0 +1,14 @@
+# kettle_boiled = True
+
+# if kettle_boiled:
+#     print("Kettle Done! Time to make Chai")
+
+customer_name = "Amit"
+customer_age = 24
+total_purchase = 1200
+
+if total_purchase > 1000:
+    print("Eligible for discount")
+    
+    if customer_age >= 60:
+        print("Senior Citizen")
---
+++ chapter_6.py
@@ -0,0 +1,18 @@
+# chai_type = "Ginger chai"
+# customer_name = "Priya"
+
+# print(f"order for {customer_name} : {chai_type} Please!")
+# chai_description ="Aromatic and Bold" 
+# print(f"First Word: {chai_description[:8]}")
+# print(f"last Word: {chai_description[12:]}")
+# print(f"step Word: {chai_description[::-1]}")
+
+# label_text = "ṇvidiā GForce RTX"
+# encoded_label = label_text.encode("utf-8")
+# print(f" encoded text:{encoded_label}")
+# print(f" Non encoded text:{label_text}")
+# decoded_lable = encoded_label.decode("utf-8")
+# print(f"decoded lable:{decoded_lable}")
+
+my_cart = ['apples', 'bananas', 'milk']
+print(f" {my_cart}")
